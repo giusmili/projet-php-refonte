6338e06dcc398b130ff759fdfd113b14d8e8e0e3
giusmili <giusmili67@gmail.com>
2023-11-07 17:46:52 +0100

    html

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-	const titre ="EDW école pour les métiers du web";
+	const titre ="EDW portfolio - développeur";
 ?>
 
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="description" content="Portfolio développeur web et applications">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php print titre ?></title>
-	
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	
 	<link rel="stylesheet" href="css/style.css">
@@ -15,9 +16,9 @@
 <body>
 
 	<ul>
-		<li><a href="index.php?page=1">&lt;/&gt;  L'école</a></li>
-		<li><a href="index.php?page=2">&lt;/&gt; Formations</a></li>
-		<li><a href="index.php?page=3">&lt;/&gt;  Contact</a></li>
+		<li><a href="index.php?page=1">études</a></li>
+		<li><a href="index.php?page=2">formations</a></li>
+		<li><a href="index.php?page=3">contact</a></li>
 		
 	</ul>
 	<main>
@@ -37,15 +38,15 @@
 						if($root==3){
 							include_once("./template/contact.html");
 						}
-						else if($root>3 || $root==0){
+						else if($root>=3 || $root<=0){
 							echo '<p class="warning"><i class="far fa-frown"></i> La page demandée n\'existe pas!!</p>';
 							//header("HTTP/1.0 404 Not Found");
 						}
 
-				}
+					}
 					else{
 						include("./template/index.html");
-					}
+				}
 
 			?>
 	</div>
