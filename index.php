18bafe7b7d52fe22a3b2d8cd1f257fbc6e9b8620
giusmili <giusmili67@gmail.com>
2023-11-07 18:33:44 +0100

    php fonction

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
-	const titre ="EDW portfolio - développeur";
-?>
-
+	include_once "./controller/controller_head.class.php";
+?> 
 <!DOCTYPE HTML>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
 	<meta name="description" content="Portfolio développeur web et applications">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php print titre ?></title>
+	<title><?= Controller_head::titre ?></title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	
 	<link rel="stylesheet" href="css/style.css">
@@ -22,33 +21,15 @@
 		
 	</ul>
 	<main>
-		<h1><?php print titre ?></h1>
+		<h1><?= Controller_head::titre ?></h1>
 
 	
 			<?php
-				if(isset($_GET['page'])){
-				//variable pour $_GET
-				$root = $_GET['page'];
-						if($root==1){
-							include_once("./template/index.html");
-						}
-						if($root==2){
-							include_once("./template/portfolio.html");
-						}
-						if($root==3){
-							include_once("./template/contact.html");
-						}
-						else if($root>=3 || $root<=0){
-							echo '<p class="warning"><i class="far fa-frown"></i> La page demandée n\'existe pas!!</p>';
-							//header("HTTP/1.0 404 Not Found");
-						}
-
-					}
-					else{
-						include("./template/index.html");
-				}
-
+				include_once "./controller/controller.class.php";
 			?>
-	</div>
+	</main>
+	<footer>
+	<p>&copy; - EDW - <?php $_now_date->dates() ?></p>
+</footer>
 </body>
 </html>
