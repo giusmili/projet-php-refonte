cc6573290e4108d528fa562fb0b29634e76e89e9
giusmili <giusmili67@gmail.com>
2024-09-14 18:42:03 +0200

    php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	const titre ="EDW portfolio - développeur";
+	const titre ="🧑‍💻 EDW portfolio - développeur";
 ?>
 
 <!DOCTYPE HTML>
@@ -26,28 +26,7 @@
 
 	
 			<?php
-				if(isset($_GET['page'])){
-				//variable pour $_GET
-				$root = $_GET['page'];
-						if($root==1){
-							include_once("./template/index.html");
-						}
-						if($root==2){
-							include_once("./template/portfolio.html");
-						}
-						if($root==3){
-							include_once("./template/contact.html");
-						}
-						else if($root>=3 || $root<=0){
-							echo '<p class="warning"><i class="far fa-frown"></i> La page demandée n\'existe pas!!</p>';
-							//header("HTTP/1.0 404 Not Found");
-						}
-
-					}
-					else{
-						include("./template/index.html");
-				}
-
+				include_once __DIR__ .'/controller/baseController.class.php';
 			?>
 	
 </body>
